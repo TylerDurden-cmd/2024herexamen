7d8913dd215141fb130008ae7f175ddf3d2d1ca1
TylerDurden-cmd <125083855+TylerDurden-cmd@users.noreply.github.com>
2024-07-29 16:04:28 +0000

    GetallenLijst

--- H1/GetallenLijst/src/App.tsx
+++ H1/GetallenLijst/src/App.tsx
@@ -10,7 +10,7 @@ const App = () => {
   const objectStudents: Students [] =[
     {
       id:0,
-      naam:"josh",
+      naam:"Josh",
       leeftijd:23,
     },{
       id:0,
@@ -34,10 +34,12 @@ const App = () => {
 
   return (
     <>
+    <h2>Getallenlijst</h2>
     <ul>
       {lijstVanGetallen.map((getal:number,index:number) => <li key={index}> {getal} </li>)}
     </ul>
-
+    
+    <h2>Studentenlijst</h2>
     <ol>
       {objectStudents.map((student:Students)=> 
       <li key={student.id}>
@@ -45,6 +47,35 @@ const App = () => {
       </li>
       )}
     </ol>
+    
+    <h2>Studentenlijst met J</h2>
+    <ol>
+      {objectStudents.filter((student) => student.naam.toLowerCase().startsWith("j")).map((student) =>
+      <li key={student.id}>{student.naam}</li>
+      )}
+    </ol>
+    <h2>Select</h2>
+    <select name="listStudents" id="">
+      <option disabled value={""}>select an option</option>
+      {objectStudents.map((student:Students,index:number)=> 
+      <option value={student.naam} key={index}>{student.naam}</option>
+    )}
+      </select>
+      
+      <h2>Table</h2>
+      <table className="tableBody">
+        <tr>
+          <th>Naam</th>
+          <th>Leeftijd</th>
+        </tr>
+          {objectStudents.map((student)=> 
+          <tr key={student.id}>
+          <td>{student.naam}</td>
+          <td>{student.leeftijd}</td>
+          </tr>
+        )}
+      </table>
+
     </>
   );
 }
