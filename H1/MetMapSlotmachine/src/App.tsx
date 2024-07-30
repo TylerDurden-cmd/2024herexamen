01fb29710a1eee1145bda18184555adbdc88f41f
TylerDurden-cmd <125083855+TylerDurden-cmd@users.noreply.github.com>
2024-07-30 17:13:56 +0000

    slotmachineMetMap

---
+++ H1/MetMapSlotmachine/src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import cherry from "/cherry.png"
+import lemon from "/lemon.png"
+import melon from "/melon.png"
+import prum from "/prum.png"
+import seven from "/seven.png"
+
+const App = () => {
+
+  const slotsImages : string[] = [cherry,lemon,melon,prum,seven];
+  const [rndNumbers,SetRndNumbers] = useState<number[]>([
+    Math.floor(Math.random() * slotsImages.length),
+    Math.floor(Math.random() * slotsImages.length),
+    Math.floor(Math.random() * slotsImages.length),
+    Math.floor(Math.random() * slotsImages.length),
+    Math.floor(Math.random() * slotsImages.length)]);
+
+    const RndGenerator = () => {
+      SetRndNumbers([Math.floor(Math.random() * slotsImages.length),
+        Math.floor(Math.random() * slotsImages.length),
+        Math.floor(Math.random() * slotsImages.length),
+        Math.floor(Math.random() * slotsImages.length),
+        Math.floor(Math.random() * slotsImages.length)])
+    }
+
+    let trueFalse: boolean = rndNumbers.every((number) => number === rndNumbers[0]);
+
+  return (
+  <>
+  {slotsImages.map((slots:string,index:number)=>
+  <img src={slots} alt="" key={index} style={{width: 150}}/>
+)}
+{trueFalse == true ? <p>Je hebt gewonnen</p> : <p>Je hebt verloren</p>}
+{rndNumbers.map((slot:number,index:number)=>
+<img src={slotsImages[slot]} alt="" style={{width: 150}}/>
+)}
+
+<input type="button" value="spin the wheel" onClick={() => RndGenerator()}/>
+  </>
+  );
+}
+
+export default App;
